1ea8b98b085fab11505be01a005c193b71c9dedb
kangyeonjin <yeonjin917n.n@gmail.com>
2024-06-25 20:01:20 +0900

    그냥 연습해봄 실행안됨

---
+++ src/main/java/WebCrawling/News.java
@@ -0,0 +1,17 @@
+package WebCrawling;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class News {
+
+    private String image;
+    private String subject;
+    private String url;
+
+
+}
---
+++ src/main/java/WebCrawling/NewsController.java
@@ -0,0 +1,22 @@
+package WebCrawling;
+
+import java.util.List;
+
+@Controller
+public class NewsController {
+
+//    private final NewsService newsService;
+//
+//    public NewsController(NewsService newsService){
+//        this.newsService = newsService;
+//
+//    }
+//
+//    public String new(Model model) throws Exception{
+//        List<News> newsList = newsService.getNewsDatas();
+//        model.addAttribute("news", newsList);
+//
+//        return "news";
+//    }
+
+}
